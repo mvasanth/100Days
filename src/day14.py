13555bdaa1db5ee7710439232ff47a534a134a9f
mvasanth <medha.vasanth@gmail.com>
2022-05-02 11:27:46 -0400

    Day 14 implementation with tests.

--- src/day14.py
+++ src/day14.py
@@ -4,7 +4,6 @@ More details of the challenge can be found here:
 https://adventofcode.com/2021/day/14
 """
 from typing import Final
-from collections import Counter
 
 def getTemplateAndRules(file):
     """
@@ -56,28 +55,13 @@ def getTemplateDict(template):
     
     return templateDict
 
-def getLetterDict(template):
-    letterDict = {}
-
-    for letter in template:
-        updateLetterDict(letterDict, letter)
-    
-    return letterDict
-
-def updateLetterDict(letterDict, letter):
-    if letter not in letterDict:
-        letterDict[letter] = 1
-    else:
-        letterDict[letter] += 1
-
 def applySteps(template, ruleDict, numSteps):
-    letterDict = getLetterDict(template)
     templateDict = getTemplateDict(template)
 
     for _ in range(numSteps):
-        (letterDict, templateDict) = applyStep(letterDict, templateDict, ruleDict)
+        templateDict = applyStep(templateDict, ruleDict)
     
-    return (letterDict, templateDict)
+    return templateDict
 
 def updatePair(pair, templateDict, count):
     if pair not in templateDict:
@@ -85,7 +69,7 @@ def updatePair(pair, templateDict, count):
     else:
         templateDict[pair] += count
     
-def applyStep(letterDict, templateDict, ruleDict):
+def applyStep(templateDict, ruleDict):
     """
     Models a single step in the polymer reaction.
     
@@ -103,9 +87,6 @@ def applyStep(letterDict, templateDict, ruleDict):
         leftPair = pair[0] + insertP
         rightPair = insertP + pair[1]
 
-        # update the newly inserted character in the letter dictionary
-        updateLetterDict(letterDict, insertP)
-
         # the old pair is broken, update it's count
         templateDictCopy[pair] -= count
 
@@ -113,23 +94,53 @@ def applyStep(letterDict, templateDict, ruleDict):
         updatePair(leftPair, templateDictCopy, count)
         updatePair(rightPair, templateDictCopy, count)
 
-    return (letterDict, templateDictCopy)
+    return templateDictCopy
+
+def getLetterDict(templateDict):
+    letterDict = {}
+
+    nonEmptyPairs = [pair for pair in templateDict.keys()
+                        if templateDict[pair] > 0]
+
+    for pair in nonEmptyPairs:
+        updateLetterDict(pair[0], letterDict, templateDict[pair])
+        updateLetterDict(pair[1], letterDict, templateDict[pair])
     
-def getPartOneResult(template):
-    counts = Counter(template)
-    mostCommonElementCount = max(counts.values())
-    leastCommonElementCount = min(counts.values())
+    for letter in letterDict:
+        if letterDict[letter] % 2 == 0:
+            letterDict[letter] //= 2
+        else:
+            letterDict[letter] = (letterDict[letter] // 2 + 1)
+
+    return letterDict
+
+def updateLetterDict(letter, letterDict, count):
+    if letter not in letterDict:
+        letterDict[letter] = count
+    else:
+        letterDict[letter] += count
+
+def getFinalResult(letterDict):
+    mostCommonElementCount = max(letterDict.values())
+    leastCommonElementCount = min(letterDict.values())
     return mostCommonElementCount - leastCommonElementCount
 
 def main():
     PAIR_INSERTION_RULES: Final = "/workspaces/advent2021/src/inputs/day14.txt"
     (template, rules) = getTemplateAndRules(PAIR_INSERTION_RULES)
+    ruleDict = getRuleDict(rules)
 
     # PART 1
-    ruleDict = getRuleDict(rules)
-    template = applySteps(template, ruleDict, 10)
-    result = getPartOneResult(template)
-    print("Result = {}".format(result))
+    templateDict = applySteps(template, ruleDict, 10)
+    letterDict = getLetterDict(templateDict)
+    result = getFinalResult(letterDict)
+    print("Result after 10 steps = {}".format(result))
+
+    # PART 2
+    templateDict = applySteps(template, ruleDict, 40)
+    letterDict = getLetterDict(templateDict)
+    result = getFinalResult(letterDict)
+    print("Result after 40 steps = {}".format(result))
 
 if __name__ == "__main__":
     main()
--- tests/day14tests.py
+++ tests/day14tests.py
@@ -5,38 +5,7 @@ sys.path.insert(1, '/workspaces/advent2021/src')
 import day14
 
 class testDay14(unittest.TestCase):
-    def testApplyStep(self):
-        templateDict = "NNCB"
-        rules = [
-            "CH -> B",
-            "HH -> N",
-            "CB -> H",
-            "NH -> C",
-            "HB -> C",
-            "HC -> B",
-            "HN -> C",
-            "NN -> C",
-            "BH -> H",
-            "NC -> B",
-            "NB -> B",
-            "BN -> B",
-            "BB -> N",
-            "BC -> B",
-            "CC -> N",
-            "CN -> C"
-        ]
-        ruleDict = day14.getRuleDict(rules)
-        self.assertEqual(16, len(ruleDict))
-        templateDict = day14.getTemplateDict("NNCB")
-        letterDict = day14.getLetterDict("NNCB")
-        (letterDict, templateDict) = day14.applyStep(letterDict, templateDict, ruleDict)
-        (letterDict, templateDict) = day14.applyStep(letterDict, templateDict, ruleDict)
-
-        templateDict = day14.getTemplateDict("NNNN")
-        templateDict = day14.applyStep(templateDict, ruleDict)
-        self.assertEqual(2, len(templateDict))
-    
-    def testDay14Part1(self):
+    def testDay14(self):
         template = "NNCB"
         rules = [
             "CH -> B",
@@ -57,11 +26,18 @@ class testDay14(unittest.TestCase):
             "CN -> C"
         ]
         ruleDict = day14.getRuleDict(rules)
+
+        # PART 1
         templateDict = day14.applySteps(template, ruleDict, 10)
-        #self.assertEqual(3073, len(template))
-        #result = day14.getPartOneResult(template)
-        #self.assertEqual(1588, result)
-        pass
+        letterDict = day14.getLetterDict(templateDict)
+        result = day14.getFinalResult(letterDict)
+        self.assertEqual(1588, result)
+
+        # PART 2
+        templateDict = day14.applySteps(template, ruleDict, 40)
+        letterDict = day14.getLetterDict(templateDict)
+        result = day14.getFinalResult(letterDict)
+        self.assertEqual(2188189693529, result)
     
 if __name__ == '__main__':
     unittest.main()
