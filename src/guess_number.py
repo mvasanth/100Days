b93b9605293d1cdd8d467665389fdaca5d1d1e44
mvasanth <medha.vasanth@gmail.com>
2022-10-08 20:00:51 +0530

    Small enhancement to the number guessing game. Added playing more than one game. Added the ability to let the user know of invalid inputs.

--- src/guess_number.py
+++ src/guess_number.py
@@ -1,12 +1,12 @@
 from art import guess_number_logo
 import random
+import os
 
 def guess_number():
     print(guess_number_logo.logo)
     print("Welcome to the number guessing game!")
     print("I'm thinking of a number between 1 and 100.")
     number = random.choice(range(1, 101))
-    print(f"Psst, the number is {number}")
     guess = 0
 
     difficulty = input("Choose a difficulty, type 'easy' or 'hard': " )
@@ -15,6 +15,9 @@ def guess_number():
         attempts = 10
     elif difficulty == "hard":
         attempts = 5
+    else:
+        print("Invalid input.")
+        return
 
     while guess != number and attempts > 0:
         print(f"You have {attempts} remaining to guess the number.")
@@ -35,4 +38,17 @@ def guess_number():
             else:
                 print("Guess again.")
 
-guess_number()
+def main_loop():
+  done = False
+  clear = lambda: os.system('clear')
+  
+  while not done:
+    game_input = input("Do you want to play a game of guessing the number? Type 'y' or 'n': ")
+  
+    if game_input == 'y':
+      clear()
+      guess_number()
+    else:
+      done = True
+
+main_loop()
