cf5675ad4338522f775a1d693ef0ae2a295a9863
mvasanth <medha.vasanth@gmail.com>
2022-10-14 11:39:19 +0530

    Completed Day 19 of the 100 Days of code challenge.

--- src/turtle_race.py
+++ src/turtle_race.py
@@ -1,47 +1,74 @@
 import random
-from turtle import Turtle, Screen, colormode
+from turtle import Turtle, Screen
 
-def get_colours():
-    color_dict = {
-        "violet": "dark violet",
-        "indigo" : "indigo",
+color_dict = {
+        "violet": "dark orchid",
+        "indigo" : "navy",
         "blue": "deep sky blue",
         "green" : "lime green",
         "yellow": "yellow",
         "orange": "dark orange",
         "red": "red"
     }
-    return list(color_dict.keys())
+
+def get_colors():
+    return (list(color_dict.keys()), list(color_dict.values()))
+
+def get_color_name(color):
+    return (list(color_dict.keys())[list(color_dict.values()).index(color)])
 
 def turtle_race():
+    """
+    Implements a turtle race game, where there are 7 turtles. Each corresponding to a color of the rainbow.
+    The user makes a bets as to which turtle will win the race. 
+    Reports if the user's bet won, and the winning turtle.
+    """
     window = Screen()
     window.setup(width=500, height=400)
-    colors = get_colours()
+    (names, colors) = get_colors()
     y_positions = [-120, -80, -40, 0, 40, 80, 120]
     is_race_on = True
     turtles = []
+    min_speed = 0
+    max_speed = 10
+    start_coord_x = -230
+    end_coord_x = 230
 
+    # Display rainbow colored turtles
     for i in range(0, len(colors)):
         t = Turtle('turtle')
         t.penup()
         t.color(colors[i])
-        t.goto(-240, y_positions[i])
+        t.goto(start_coord_x, y_positions[i])
         turtles.append(t)
     
-    guess = window.textinput("Make a bet", "Which turtle will win? Enter a color of the rainbow:")
-    print(guess)
+    # ASk the user for a bet
+    guess = window.textinput("Make a bet!", "Which turtle will win? Enter a color:")
+    print(f"You are betting on turtle: {guess}.")
 
-    if not guess in colors:
+    # Do not start the game prematurely
+    if not guess in names:
         is_race_on = False
     else:
+        # Turtle race begins!
         while is_race_on:
             for turtle in turtles:
-                speed = random.randint(1, 10)
+                speed = random.randint(min_speed, max_speed)
                 turtle.forward(speed)
 
-                if turtle.xcor() > 230:
-                    print("This turtle has won")
+                if turtle.xcor() > end_coord_x:
+                    # turtle.color() returns a tuple of colors.
+                    # turtle.color()[0] corresponds to the pencolor, the other is the fillcolor.
+                    winning_turtle = turtle.color()[0]
                     is_race_on = False
+
+        # Winner evaluation
+        winning_turtle_name = get_color_name(winning_turtle)
+
+        if guess == winning_turtle_name:
+            print(f"You won! The {winning_turtle_name} indeed won the race!")
+        else:
+            print(f"You lose. The {winning_turtle_name} won the race!")
 
     window.exitonclick()
 
